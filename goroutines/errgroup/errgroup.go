7c71136be96cf29a9d1e6b70ad2951e439f1b67e
Milad Rasouli <milad.rasouli@tuta.io>
2025-12-12 12:34:57 +0330

    feat: errgroup patterns

---
+++ goroutines/errgroup/errgroup.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"net/http"
+	"runtime"
+)
+
+func main() {
+	urls := []string{
+		"http://www.google0.com",
+		"http://www.golang0.org",
+		"http://www.somestupidname0.com",
+	}
+
+	var g errgroup.Group
+	g.SetLimit(1)
+
+	for _, url := range urls {
+		g.Go(func() error {
+			_, err := http.Get(url)
+			if err == nil {
+				fmt.Printf("Successfully fetched %s\n", url)
+			}
+			return fmt.Errorf("error fetching %s: %v", url, err)
+		})
+	}
+	println("num: ", runtime.NumGoroutine())
+
+	if err := g.Wait(); err == nil {
+		fmt.Println("Successfully fetched all URLs.")
+	} else {
+		fmt.Printf("Failed to fetch a URL: %v\n", err)
+	}
+}
---
+++ goroutines/errgroup/go.mod
@@ -0,0 +1,5 @@
+module github.com/MiladCodeLab/golang
+
+go 1.25.1
+
+require golang.org/x/sync v0.19.0
